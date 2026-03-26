bee87d8ebad5c89591e0f8e8c506d85247beaff1
JL <jere.lehtimki@gmail.com>
2026-03-26 11:14:01 +0200

    KT9 tapahtumasuodattimet tehty

---
+++ KT9-eventFiltering/src/main.rs
@@ -0,0 +1,40 @@
+#![allow(dead_code, unused_variables, unused_imports, unused_mut)]
+
+mod events;
+mod birthday;
+mod providers;
+mod filters;
+
+use chrono::{NaiveDate};
+use crate::providers::{sqlite::SQLiteProvider};
+use crate::providers::{EventProvider, SimpleProvider, my_provider::MyProvider};
+use crate::events::{Event, Category};
+use std::path::{PathBuf, Path};
+use std::fs;
+use dirs;
+use KT9_eventFiltering::Config;
+
+fn get_config_path(app_name: &str) -> Option<PathBuf> {
+    if let Some(config_dir) = dirs::config_dir() {
+        let config_path = config_dir.join(app_name);
+        if !config_path.exists() {
+            if let Err(_) = fs::create_dir(&config_path) {
+                eprintln!("Unable to create config directory for {}", app_name);
+                return None;
+            }
+        }
+        return Some(config_path);
+    }
+    None
+}
+
+fn main() {
+    let provider = SQLiteProvider::new("sqlitedb", Path::new("C:/Users/jerel/AppData/Roaming/today/mydatabase.db"));
+    let mut events: Vec<Event> = Vec::new();
+
+    provider.get_events(&mut events);
+
+    for event in &events {
+        println!("{}", event);
+    }
+}
